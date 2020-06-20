db6a7599cbe2dc8f4e6ab06961f724e52bff5e5f
teebermoon <67171844+teebermoon@users.noreply.github.com>
2020-06-20 14:44:56 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="space-remover-teeber", 
+    version="0.0.1",
+    author="teeber",
+    description="A program to remove spaces from a text file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/teebermoon/Srem
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
